b4bdc1e359018d4476b2db1fc196114fccaa24ba
nishidayoshikatsu <nsd24technology@gmail.com>
2020-04-25 06:18:09 +0900

    新しい相談件数データを読み込めるようにした

--- components/cards/ConsultationDeskReportsNumberCard.vue
+++ components/cards/ConsultationDeskReportsNumberCard.vue
@@ -5,7 +5,7 @@
       :title-id="'number-of-reports-to-covid19-consultation-desk'"
       :chart-id="'time-bar-chart-querents'"
       :chart-data="querentsGraph"
-      :date="Data.querents.date"
+      :date="Querents.last_update"
       :unit="$t('件.reports')"
       :url="
         'https://www.pref.yamaguchi.lg.jp/cms/a15200/kansensyou/ncorona.html'
@@ -23,42 +23,12 @@
     "件": {
       "reports": "件"
     }
-  },
-  "en": {
-    "新型コロナ受診相談窓口相談件数": "Number of inquiries to combined telephone advisory center",
-    "件": {
-      "reports": "reports"
-    }
-  },
-  "zh-cn": {
-    "新型コロナ受診相談窓口相談件数": "新冠肺炎就诊咨询窗口的咨询数",
-    "件": {
-      "reports": "件"
-    }
-  },
-  "zh-tw": {
-    "新型コロナ受診相談窓口相談件数": "就診窗口諮詢數",
-    "件": {
-      "reports": "件"
-    }
-  },
-  "ko": {
-    "新型コロナ受診相談窓口相談件数": "코로나19 진찰 상담 창구 상담 건수",
-    "件": {
-      "reports": "건"
-    }
-  },
-  "ja-basic": {
-    "新型コロナ受診相談窓口相談件数": "コロナのことで とうきょうと に そうだんした ひとの かず",
-    "件": {
-      "reports": "けん"
-    }
   }
 }
 </i18n>
 
 <script>
-import Data from '@/data/data.json'
+import Querents from '@/data/querents.json'
 import formatGraph from '@/utils/formatGraph'
 import TimeBarChartOnlyAll2 from '@/components/TimeBarChart_only-all2.vue'
 
@@ -68,10 +38,10 @@ export default {
   },
   data() {
     // 帰国者・接触者電話相談センター相談件数
-    const querentsGraph = formatGraph(Data.querents.data)
+    const querentsGraph = formatGraph(Querents.data)
 
     const data = {
-      Data,
+      Querents,
       querentsGraph
     }
     return data
