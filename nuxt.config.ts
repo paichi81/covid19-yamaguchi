163d3f2d1ebb6966c57c77f802c3f3944c9ec97a
nishidayoshikatsu <nsd24technology@gmail.com>
2020-06-22 03:40:12 +0900

    google analyticsの導入

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -81,7 +81,11 @@ const config: Configuration = {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'apple-touch-icon', href: '/apple-touch-icon-precomposed.png' }
+      { rel: 'apple-touch-icon', href: '/apple-touch-icon-precomposed.png' },
+      {
+        rel: 'stylesheet',
+        href: 'https://use.fontawesome.com/releases/v5.6.1/css/all.css'
+      }
     ]
   },
   /*
@@ -148,7 +152,7 @@ const config: Configuration = {
     }
   },
   googleAnalytics: {
-    id: process.env.GOOGLE_ANALYTICS_ID // .env.production などに設定してください。
+    id: 'UA-161156836-1'
   },
   optionalCookies: [
     {
