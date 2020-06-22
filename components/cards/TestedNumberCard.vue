36f7fe449418f2c37136ad58ac6dcc7663c72f06
nishidayoshikatsu <nsd24technology@gmail.com>
2020-06-22 22:26:55 +0900

    PCR検査実施件数のレイアウトを修正

--- components/cards/TestedNumberCard.vue
+++ components/cards/TestedNumberCard.vue
@@ -1,40 +1,41 @@
 <template>
   <v-col cols="12" md="6" class="DataCard">
-    <time-bar-chart-only-all
+    <time-bar-chart
       :title="$t('PCR検査実施件数（累計）')"
       :title-id="'number-of-tested'"
       :chart-id="'time-stacked-bar-chart-inspections'"
       :chart-data="inspectionsGraph"
       :date="Inspections.last_update"
+      :show-button="false"
       :unit="$t('件.tested')"
       :url="
         'https://yamaguchi-opendata.jp/ckan/dataset/350001-covid19'
       "
-    />
+    >
+      <template v-slot:description>
+        <ul>
+          <li>
+            {{ $t('（注）同一の対象者について複数の検体を調査する場合あり') }}
+          </li>
+          <li>
+            {{ $t('（注）3月9日以前は検査実施件数か検査人数か不明') }}
+          </li>
+        </ul>
+      </template>
+    </time-bar-chart>
+
     <!-- 件.tested = 検査数 -->
   </v-col>
 </template>
 
-<!-- i18n>
-{
-  "ja": {
-	"検査実施件数": "検査実施件数",
-	"累計": "累計",
-    "件": {
-      "tested": "件"
-    }
-  }
-}
-</i18n -->
-
 <script>
 //import Inspections from '@/data/inspections.json'
 import formatGraph from '@/utils/formatGraph'
-import TimeBarChartOnlyAll from '@/components/TimeBarChart_only-all.vue'
+import TimeBarChart from '@/components/TimeBarChart.vue'
 
 export default {
   components: {
-    TimeBarChartOnlyAll
+    TimeBarChart
   },
   data() {
 	const Inspections = this.$store.state.data.inspections
