ece9e81a5196c9d49b98aa0136e1ebee9a877ec7
nishidayoshikatsu <nsd24technology@gmail.com>
2020-06-23 06:46:09 +0900

    利用しない部分を削除

--- pages/cards/_card.vue
+++ pages/cards/_card.vue
@@ -2,146 +2,88 @@
   <div>
     <confirmed-cases-details-card
       v-if="this.$route.params.card == 'details-of-confirmed-cases'"
-    />
-    <tested-cases-details-card
-      v-else-if="this.$route.params.card == 'details-of-tested-cases'"
-    />
+    /><!-- ok -->
     <confirmed-cases-number-card
       v-else-if="this.$route.params.card == 'number-of-confirmed-cases'"
-    />
-    <confirmed-cases-by-municipalities-card
-      v-else-if="
-        this.$route.params.card == 'number-of-confirmed-cases-by-municipalities'
-      "
-    />
+    /><!-- ok -->
     <confirmed-cases-attributes-card
       v-else-if="this.$route.params.card == 'attributes-of-confirmed-cases'"
-    />
+    /><!-- ok -->
     <tested-number-card
       v-else-if="this.$route.params.card == 'number-of-tested'"
-    />
-    <inspection-persons-number-card
-      v-else-if="this.$route.params.card == 'number-of-inspection-persons'"
-    />
-    <telephone-advisory-reports-number-card
-      v-else-if="
-        this.$route.params.card ==
-          'number-of-reports-to-covid19-telephone-advisory-center'
-      "
-    />
+    /><!-- ok -->
     <consultation-desk-reports-number-card
       v-else-if="
         this.$route.params.card ==
           'number-of-reports-to-covid19-consultation-desk'
       "
-    />
-    <metro-card
+    /><!-- ok -->
+    <yamaguchi-map-card
       v-else-if="
-        this.$route.params.card == 'predicted-number-of-toei-subway-passengers'
+        this.$route.params.card ==
+          'map-in-yamaguchi'
       "
-    />
-    <agency-card v-else-if="this.$route.params.card == 'agency'" />
-    <positive-number-by-diagnosed-date-card
-      v-else-if="this.$route.params.card == 'positive-number-by-diagnosed-date'"
-    />
-    <positive-rate-card
-      v-else-if="this.$route.params.card == 'positive-rate'"
     />
   </div>
 </template>
 
 <script>
-import Data from '@/data/data.json'
+//import Data from '@/data/data.json'
 // import MetroData from '@/data/metro.json'
 // import agencyData from '@/data/agency.json'
 // ↓ import patientData from '@/data/patient.json'
-import patientData from '@/data/patients.json'
+//import patientData from '@/data/patients.json'
 // import PositiveByDiagnosedData from '@/data/positive_by_diagnosed.json'
 // import PositiveRate from '@/data/positive_rate.json'
 import ConfirmedCasesDetailsCard from '@/components/cards/ConfirmedCasesDetailsCard.vue'
-import TestedCasesDetailsCard from '@/components/cards/TestedCasesDetailsCard.vue'
 import ConfirmedCasesNumberCard from '@/components/cards/ConfirmedCasesNumberCard.vue'
 import ConfirmedCasesAttributesCard from '@/components/cards/ConfirmedCasesAttributesCard.vue'
 // import ConfirmedCasesByMunicipalitiesCard from '@/components/cards/ConfirmedCasesByMunicipalitiesCard.vue'
 import TestedNumberCard from '@/components/cards/TestedNumberCard.vue'
-import InspectionPersonsNumberCard from '@/components/cards/InspectionPersonsNumberCard.vue'
+//import InspectionPersonsNumberCard from '@/components/cards/InspectionPersonsNumberCard.vue'
 // import TelephoneAdvisoryReportsNumberCard from '@/components/cards/TelephoneAdvisoryReportsNumberCard.vue'
 import ConsultationDeskReportsNumberCard from '@/components/cards/ConsultationDeskReportsNumberCard.vue'
 // import MetroCard from '@/components/cards/MetroCard.vue'
 // import AgencyCard from '@/components/cards/AgencyCard.vue'
 // import PositiveNumberByDiagnosedDateCard from '@/components/cards/PositiveNumberByDiagnosedDateCard.vue'
 // import PositiveRateCard from '@/components/cards/PositiveRateCard.vue'
+import YamaguchiMapCard from '@/components/cards/YamaguchiMapCard.vue'
 
 export default {
   components: {
-    PositiveRateCard,
     ConfirmedCasesDetailsCard,
-    TestedCasesDetailsCard,
     ConfirmedCasesNumberCard,
     ConfirmedCasesAttributesCard,
-    ConfirmedCasesByMunicipalitiesCard,
     TestedNumberCard,
-    InspectionPersonsNumberCard,
-    TelephoneAdvisoryReportsNumberCard,
     ConsultationDeskReportsNumberCard,
-    MetroCard,
-    AgencyCard,
-    PositiveNumberByDiagnosedDateCard
+    YamaguchiMapCard
   },
   data() {
     let title, updatedAt
     switch (this.$route.params.card) {
       case 'details-of-confirmed-cases':
         title = this.$t('検査陽性者の状況')
-        updatedAt = Data.inspections_summary.date
-        break
-      case 'details-of-tested-cases':
-        title = this.$t('検査実施状況')
-        updatedAt = Data.inspection_status_summary.date
+        updatedAt = this.$store.state.data.hospitalizations.last_update
         break
       case 'number-of-confirmed-cases':
         title = this.$t('陽性患者数')
-        updatedAt = Data.patients.date
-        break
-      case 'number-of-confirmed-cases-by-municipalities':
-        title = this.$t('陽性患者数（区市町村別）')
-        updatedAt = patientData.date
+        updatedAt = this.$store.state.data.patientscnt.last_update
         break
       case 'attributes-of-confirmed-cases':
         title = this.$t('陽性患者の属性')
-        updatedAt = Data.patients.date
+        updatedAt = this.$store.state.data.patients.last_update
         break
       case 'number-of-tested':
-        title = this.$t('検査実施件数')
-        updatedAt = Data.inspections_summary.date
-        break
-      case 'number-of-inspection-persons':
-        title = this.$t('検査実施人数')
-        updatedAt = Data.inspection_persons.date
-        break
-      case 'number-of-reports-to-covid19-telephone-advisory-center':
-        title = this.$t('新型コロナコールセンター相談件数')
-        updatedAt = Data.contacts.date
+        title = this.$t('PCR検査実施件数（累計）')
+        updatedAt = this.$store.state.data.inspections.last_update
         break
       case 'number-of-reports-to-covid19-consultation-desk':
-        title = this.$t('新型コロナ受診相談窓口相談件数')
-        updatedAt = Data.querents.date
+        title = this.$t('新型コロナ受診相談窓口相談件数（累計）')
+        updatedAt = this.$store.state.data.querents.last_update
         break
-      case 'predicted-number-of-toei-subway-passengers':
-        title = this.$t('都営地下鉄の利用者数の推移')
-        updatedAt = MetroData.date
-        break
-      case 'agency':
-        title = this.$t('都庁来庁者数の推移')
-        updatedAt = agencyData.date
-        break
-      case 'positive-number-by-diagnosed-date':
-        title = this.$t('陽性患者数（検査結果判明日別）')
-        updatedAt = PositiveByDiagnosedData.date
-        break
-      case 'positive-rate':
-        title = this.$t('検査実施人数（陰性確認を除く）と陽性率の推移')
-        updatedAt = PositiveRate.date
+      case 'map-in-yamaguchi':
+        title = this.$t('市区町村別陽性患者の累計')
+        updatedAt = this.$store.state.data.mapupdate
         break
     }
 
@@ -152,14 +94,14 @@ export default {
     return data
   },
   head() {
-    const url = 'https://stopcovid19.metro.tokyo.lg.jp'
+    const url = 'https://yamaguchi.stopcovid19.jp'
     const timestamp = new Date().getTime()
     const ogpImage =
       this.$i18n.locale === 'ja'
         ? `${url}/ogp/${this.$route.params.card}.png?t=${timestamp}`
         : `${url}/ogp/${this.$i18n.locale}/${this.$route.params.card}.png?t=${timestamp}`
     const description = `${this.updatedAt} | ${this.$t(
-      '当サイトは新型コロナウイルス感染症 (COVID-19) に関する最新情報を提供するために、東京都が開設したものです。'
+      '当サイトは新型コロナウイルス感染症 (COVID-19) に関する最新情報を提供するために、山口県内外の有志が開設したものです。'
     )}`
 
     return {
@@ -176,7 +118,7 @@ export default {
           content:
             this.title +
             ' | ' +
-            this.$t('東京都') +
+            this.$t('山口県公認') +
             ' ' +
             this.$t('新型コロナウイルス感染症') +
             this.$t('対策サイト')
