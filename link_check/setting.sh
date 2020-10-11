73bb476664756fb93609991346723ae7a862879f
Taiki Tomita <g.tomita.t@gmail.com>
2020-10-11 13:49:26 +0000

    slack bot再インストールしたらwebhookのURLが変わってしまったので再度紐づけ

--- link_check/setting.sh
+++ link_check/setting.sh
@@ -1,2 +1,2 @@
-HOOK_URL=https://hooks.slack.com/services/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
+HOOK_URL=https://hooks.slack.com/services/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
 TARGET_DOMAIN=yamaguchi.stopcovid19.jp
