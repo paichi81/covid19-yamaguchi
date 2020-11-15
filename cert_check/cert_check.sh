ffba7661766521148018fe8951af1bf2534a74a8
Taiki Tomita <g.tomita.t@gmail.com>
2020-11-15 15:15:11 +0900

    証明書が切れていた場合にslackにアラートするようにした．

--- cert_check/cert_check.sh
+++ cert_check/cert_check.sh
@@ -1,19 +1,42 @@
+source setting.sh
 
-domain=yamaguchi.stopcovid19.jp
-dates=$(openssl s_client -connect $domain:443 </dev/null 2>/dev/null|openssl x509 -noout -dates ) 
+function cert_check(){
+	dates=$(openssl s_client -connect $1:443 </dev/null 2>/dev/null|openssl x509 -noout -dates ) 
 
-notBefore=$(echo $dates|sed -e "s/notBefore=\([^=]*\)notAfter=\(.*\)/\1/")
-notAfter=$(echo $dates|sed -e "s/notBefore=\([^=]*\)notAfter=\(.*\)/\2/")
+	notBefore=$(echo $dates|sed -e "s/notBefore=\([^=]*\)notAfter=\(.*\)/\1/")
+	notAfter=$(echo $dates|sed -e "s/notBefore=\([^=]*\)notAfter=\(.*\)/\2/")
 
-nb=$(date --date="$notBefore" +%s)
-n=$(date +%s)
-na=$(date --date="$notAfter" +%s)
+	nb=$(date --date="$notBefore" +%s)
+	n=$(date +%s)
+	na=$(date --date="$notAfter" +%s)
 
-if [ $n -lt $nb -o $na -lt $n ]; then
-	echo "Expired certification detected!!"
+	if [ $n -lt $nb -o $na -lt $n ]; then
+		echo "Expired certification detected!!"
+		return 1;
+	else
+		echo "Certification is not expired."
+		return 0;
+	fi
+}
+
+function send_message(){
+	MESSAGE=$(cat /dev/stdin | sed -e 's/"/\\\"/g')
+	HOOK_URL=$1
+	curl \
+		-X POST\
+		-H 'Content-type: application/json'\
+		--data "{\"text\":\"$MESSAGE\"}"\
+		$HOOK_URL
+}
+
+#====MAIN======
+if [ "$HOOK_URL" == "" ]; then
+	echo "ERROR: HOOK_URL is empty."
+	echo "Please create slack incoming webhook and assign its url to HOOK_URL in setting.sh."
 	exit 1;
-else
-	echo "Certification is not expired."
-	exit 0;
 fi
-
+if ! cert_check $TARGET_DOMAIN; then
+	send_message $HOOK_URL << EOS
+WARNING: Expired certification detected!!
+EOS
+fi
---
+++ cert_check/setting.sh
@@ -0,0 +1,2 @@
+HOOK_URL=
+TARGET_DOMAIN=yamaguchi.stopcovid19.jp
