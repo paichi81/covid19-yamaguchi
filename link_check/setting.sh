c642be9627b8850cad413bab17b71a9641314b60
nishidayoshikatsu <nsd24technology@gmail.com>
2021-02-20 17:44:25 +0900

    covid19-yamaguchiのslackのwebhookURLに変更

--- link_check/setting.sh
+++ link_check/setting.sh
@@ -1,2 +1,2 @@
-HOOK_URL=https://hooks.slack.com/services/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
+HOOK_URL=https://hooks.slack.com/services/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
 TARGET_DOMAIN=yamaguchi.stopcovid19.jp
